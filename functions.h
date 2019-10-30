cdbf703530d847dc637980203794a51325a15ad7
LeoShestakov <leo.shestakov@hotmail.com>
2019-10-30 14:33:49 -0400

    header

--- functions.h
+++ functions.h
@@ -1,4 +1,8 @@
-struct node { int i; struct node *next; };
+struct node{ 
+  char name[100];
+  char artist[100];
+  struct song_node *next;
+};
 
 void print_list(struct node *head);
 struct node * insert_front(struct node *head, int atFront);
