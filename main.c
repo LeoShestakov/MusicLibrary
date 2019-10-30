657f35dcee2eb486a55a086c128e55a3edf137a0
LeoShestakov <leo.shestakov@hotmail.com>
2019-10-30 14:50:01 -0400

    main

--- main.c
+++ main.c
@@ -3,49 +3,51 @@
 #include<stdlib.h>
 
 int main() {
-	struct node *top = malloc(sizeof(struct node));
-	
+	struct node *top = NULL;
+
 	printf("Printing empty: ");
 	print_list(top);
 	printf("\n");
-	
+
 	printf("-----\n");
-	
-	int i;
+
 	// Populating list
-	for (i = 9; i >= 0; i--) {
-		top = insert_front(top, i);
-		printf("Printing list (added %d to front): ", i);
-		print_list(top);
-		printf("\n");
+	char* name;
+	char* artist;
+	name = "LAUDER"
+	artist = "JID"
+	top = insert(top, name, artist);
+	printf("Printing list (added %s by %s): ", name, artist);
+	print_list(top);
+	printf("\n");
 	}
-	
+
 	printf("-----\n");
-	
-	top = remove_node(top, 7);
-	printf("Printing list (removed 7): ");
-	print_list(top);
-	printf("\n");
-	
-	top = remove_node(top, 0);
-	printf("Printing list (removed 0): ");
-	print_list(top);
-	printf("\n");
-	
-	top = remove_node(top, 9);
-	printf("Printing list (removed 9): ");
-	print_list(top);
-	printf("\n");
-	
-	top = remove_node(top, 9);
-	printf("Printing list (tried removing 9 again): ");
-	print_list(top);
-	printf("\n");
-	
-	printf("-----\n");
-	
-	free_list(top);
-	printf("List after freeing all nodes: ");
-	print_list(top);
-	printf("\n");
+
+	// top = remove_node(top, 7);
+	// printf("Printing list (removed 7): ");
+	// print_list(top);
+	// printf("\n");
+	//
+	// top = remove_node(top, 0);
+	// printf("Printing list (removed 0): ");
+	// print_list(top);
+	// printf("\n");
+	//
+	// top = remove_node(top, 9);
+	// printf("Printing list (removed 9): ");
+	// print_list(top);
+	// printf("\n");
+	//
+	// top = remove_node(top, 9);
+	// printf("Printing list (tried removing 9 again): ");
+	// print_list(top);
+	// printf("\n");
+	//
+	// printf("-----\n");
+	//
+	// free_list(top);
+	// printf("List after freeing all nodes: ");
+	// print_list(top);
+	// printf("\n");
 }
