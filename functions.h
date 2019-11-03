507ceab59727b1b8b04d1d3b609fee49ad88eb77
athompson00 <athompson00@stuy.edu>
2019-11-03 18:10:55 -0500

    added stuff to headers

--- functions.h
+++ functions.h
@@ -1,4 +1,4 @@
-struct node{ 
+struct node{
   char name[100];
   char artist[100];
   struct song_node *next;
@@ -9,5 +9,8 @@ struct node * makeSong(char *name, char *artist);
 struct node * insert_front(struct node *head, struct node *toInsert);
 int songcmp(struct node *songA, struct node *songB);
 struct node * insert(struct node *head, struct node *toInsert);
+struct node * getSong(struct node *head, char *artist, char *name);
+struct node * getSongByArtist(struct node *head, char *artist);
+struct node * getRandSong(struct node *head);
 // struct node * free_list(struct node *head);
 // struct node * remove_node(struct node *head, int data);
